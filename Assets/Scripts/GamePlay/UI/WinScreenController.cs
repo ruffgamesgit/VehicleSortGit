9c4ac5788f721c26abc6c2033f0ef797d86b48f4
fatihruffgames <164330349+fatihruffgames@users.noreply.github.com>
2024-05-20 09:48:40 +0300

    Added: new confetti particle & game icon

--- Assets/Scripts/GamePlay/UI/WinScreenController.cs
+++ Assets/Scripts/GamePlay/UI/WinScreenController.cs
@@ -1,25 +1,22 @@
-using System;
-
+using AssetKits.ParticleImage;
 using Core.Locator;
 using Core.Services.GamePlay;
-using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using Services.Sound;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace GamePlay.UI
 {
     public class WinScreenController : MonoBehaviour
     {
-        [SerializeField]private CanvasGroup canvasGroup;
+        [SerializeField] private CanvasGroup canvasGroup;
         [SerializeField] private Button nextButton;
-        [SerializeField] private GameObject confetti;
-        
+        [SerializeField] private ParticleImage confetti;
+
         private IGamePlayService _gamePlayService;
         private ISoundService _soundService;
-        
+
         private void Awake()
         {
             _gamePlayService = ServiceLocator.Instance.Resolve<IGamePlayService>();
@@ -32,14 +29,16 @@ namespace GamePlay.UI
             canvasGroup.blocksRaycasts = true;
             canvasGroup.interactable = true;
             var sequence = DOTween.Sequence();
-            sequence.Insert(1f,canvasGroup.DOFade(1, 0.5f));
-            sequence.InsertCallback(0.5f,() =>
+            sequence.Insert(1f, canvasGroup.DOFade(1, 0.5f));
+            sequence.InsertCallback(0.5f, () =>
             {
-                if(confetti != null)
-                    confetti.SetActive(true);
+                if (confetti != null)
+                {
+                    confetti.Play();
+                }
+
                 _soundService.PlaySound(SoundTypeEnum.WinSound);
             });
-            
         }
 
         private void NextButtonClick()
